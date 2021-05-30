4b218c8c64fe93371ce9fc69d76254e85489eb63
Eyadzz <eyadyoussef1336@gmail.com>
2021-05-30 20:59:44 +0200

    updated

--- src/LinkedAllocation.java
+++ src/LinkedAllocation.java
@@ -8,56 +8,7 @@ public class LinkedAllocation {
     private String blocks;
     private String fileName;
 
-    public static class LinkedList {
 
-        Node head;
-        static class Node
-        {
-            int position;
-            Node next;
-
-            Node(int pos)
-            {
-                position = pos;
-                next = null;
-            }
-        }
-
-        public static LinkedList insert(LinkedList list, int data)
-        {
-            Node new_node = new Node(data);
-            new_node.next = null;
-
-            if (list.head == null)
-            {
-                list.head = new_node;
-            }
-            else
-            {
-                Node last = list.head;
-                while (last.next != null)
-                {
-                    last = last.next;
-                }
-
-                last.next = new_node;
-            }
-
-            return list;
-        }
-
-        public static void printList(LinkedList list)
-        {
-            Node currNode = list.head;
-            System.out.print("File Indexes: ");
-
-            while (currNode != null)
-            {
-                System.out.print(currNode.position + " ");
-                currNode = currNode.next;
-            }
-        }
-    }
 
     public LinkedAllocation() throws IOException
     {
@@ -81,31 +32,26 @@ public class LinkedAllocation {
         parent = Directory.checkPath(path, root);
         String[] pathSections = path.split("/");
         String newFileName = pathSections[pathSections.length-1];
-        LinkedList file = new LinkedList();
         file f = new file();
         f.setName(pathSections[1]);
         ArrayList<Integer> allocatedBlocks = new ArrayList<>();
-        if(parent != null && !parent.fileExist(newFileName))
-        {
-                for (int i = 0; i < size; i++)
-                {
-                    for (int j = 0; j < blocks.length(); j++)
-                    {
+
+                for (int i = 0; i < size; i++) {
+                    for (int j = 0; j < blocks.length(); j++) {
                         String bit = String.valueOf(blocks.charAt(j));
 
-                        if (bit.equalsIgnoreCase("0"))
-                        {
-                            file.insert(file, j);
+                        if (bit.equalsIgnoreCase("0")) {
+                            System.out.println(j);
+                            f.linkedList.insert(f.linkedList, j);
                             allocatedBlocks.add(j);
                             blocks = blocks.replaceFirst("0", "1");
                             break;
                         }
                     }
                 }
-
-        }
+        f.linkedList.printList(f.linkedList);
         f.setAllocatedBlocks(allocatedBlocks);
-        file.printList(file);
+
         //root.addFile(file);
     }
     public void deleteFile(String path)
--- src/file.java
+++ src/file.java
@@ -3,15 +3,18 @@ import java.util.ArrayList;
 public class file {
     private String name;
     private ArrayList<Integer>allocatedBlocks;
-
+    public static LinkedList linkedList;
     public file(String path, String name, ArrayList<Integer> allocatedBlocks) {
         this.allocatedBlocks = allocatedBlocks;
         this.name = name;
     }
 
-    public file() {
+    public file()
+    {
         allocatedBlocks = new ArrayList<Integer>();
+        linkedList = new LinkedList();
     }
+
 
 
     public String getName() {
@@ -28,5 +31,54 @@ public class file {
 
     public void setAllocatedBlocks(ArrayList<Integer> allocatedBlocks) {
         this.allocatedBlocks = allocatedBlocks;
+    }
+
+    public static class LinkedList {
+
+        Node head;
+        static class Node
+        {
+            int position;
+            Node next;
+
+            Node(int pos)
+            {
+                position = pos;
+                next = null;
+            }
+        }
+
+        public static void insert(LinkedList list, int data)
+        {
+            Node new_node = new Node(data);
+            new_node.next = null;
+
+            if (list.head == null)
+            {
+                list.head = new_node;
+            }
+            else
+            {
+                Node last = list.head;
+                while (last.next != null)
+                {
+                    last = last.next;
+                }
+
+                last.next = new_node;
+            }
+        }
+
+        public static void printList(LinkedList list)
+        {
+            Node currNode = list.head;
+            System.out.print("File Indexes: ");
+
+            while (currNode != null)
+            {
+                System.out.print(currNode.position + " ");
+                currNode = currNode.next;
+            }
+        }
     }
 }
